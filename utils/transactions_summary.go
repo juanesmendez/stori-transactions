aaa759b06432650dc5ea2b45ee42ef07bd0dd732
Juan.Mendez <juan.mendez@rappi.com>
2024-03-14 00:00:08 -0500

    Created nested functions in transactions summary logic.

--- utils/transactions_summary.go
+++ utils/transactions_summary.go
@@ -5,6 +5,19 @@ import (
 	"time"
 )
 
+type MonthlyTransactionSummary struct {
+	TransactionsCount int
+	Total             float64
+	Average           float64
+	DataByType        map[model.TransactionType]SummaryTransactionType
+}
+
+type SummaryTransactionType struct {
+	TransactionsCount int
+	Total             float64
+	Average           float64
+}
+
 func GetBalance(transactions []model.Transaction) float64 {
 	var balance float64
 
@@ -16,48 +29,9 @@ func GetBalance(transactions []model.Transaction) float64 {
 }
 
 func GroupTransactionsByMonth(transactions []model.Transaction) map[time.Month]MonthlyTransactionSummary {
-	transSummaryByMonth := make(map[time.Month]MonthlyTransactionSummary)
+	transactionsSummaryByMonth := getTransactionsSummaryByMonth(transactions)
 
-	for _, transaction := range transactions {
-		if _, ok := transSummaryByMonth[transaction.Date.Month()]; !ok {
-			transSummaryByMonth[transaction.Date.Month()] = MonthlyTransactionSummary{
-				DataByType: make(map[model.TransactionType]SummaryTransactionType),
-			}
-		}
-
-		monthSummary := transSummaryByMonth[transaction.Date.Month()]
-		monthSummary.TransactionsCount++
-		monthSummary.Total += transaction.Value
-
-		dataByType := monthSummary.DataByType[transaction.Type]
-		dataByType.TransactionsCount++
-		dataByType.Total += transaction.Value
-
-		monthSummary.DataByType[transaction.Type] = dataByType
-
-		transSummaryByMonth[transaction.Date.Month()] = monthSummary
-	}
-
-	for month, summary := range transSummaryByMonth {
-		average := 0.0
-		if summary.TransactionsCount > 0 {
-			average = summary.Total / float64(summary.TransactionsCount)
-		}
-		summary.Average = average
-
-		monthlyAverage := 0.0
-		for transType, transTypeData := range summary.DataByType {
-			if transTypeData.TransactionsCount > 0 {
-				monthlyAverage = transTypeData.Total / float64(transTypeData.TransactionsCount)
-			}
-			transTypeData.Average = monthlyAverage
-			summary.DataByType[transType] = transTypeData
-		}
-
-		transSummaryByMonth[month] = summary
-	}
-
-	return transSummaryByMonth
+	return calculateMonthlyAvgValueByTransType(transactionsSummaryByMonth)
 }
 
 func AverageByType(transactions []model.Transaction) map[model.TransactionType]float64 {
@@ -82,16 +56,51 @@ func AverageByType(transactions []model.Transaction) map[model.TransactionType]f
 	return averageByTransactionType
 }
 
-type MonthlyTransactionSummary struct {
-	TransactionsCount int
-	Total             float64
-	Average           float64
-	DataByType        map[model.TransactionType]SummaryTransactionType
+func calculateMonthlyAvgValueByTransType(transSummaryByMonth map[time.Month]MonthlyTransactionSummary) map[time.Month]MonthlyTransactionSummary {
+	for month, summary := range transSummaryByMonth {
+		average := 0.0
+		if summary.TransactionsCount > 0 {
+			average = summary.Total / float64(summary.TransactionsCount)
+		}
+		summary.Average = average
+
+		monthlyAverage := 0.0
+		for transType, transTypeData := range summary.DataByType {
+			if transTypeData.TransactionsCount > 0 {
+				monthlyAverage = transTypeData.Total / float64(transTypeData.TransactionsCount)
+			}
+			transTypeData.Average = monthlyAverage
+			summary.DataByType[transType] = transTypeData
+		}
+
+		transSummaryByMonth[month] = summary
+	}
+
+	return transSummaryByMonth
 }
 
-// FIXME Mover esta struct de aquí
-type SummaryTransactionType struct {
-	TransactionsCount int
-	Total             float64
-	Average           float64
+func getTransactionsSummaryByMonth(transactions []model.Transaction) map[time.Month]MonthlyTransactionSummary {
+	transSummaryByMonth := make(map[time.Month]MonthlyTransactionSummary)
+
+	for _, transaction := range transactions {
+		if _, ok := transSummaryByMonth[transaction.Date.Month()]; !ok {
+			transSummaryByMonth[transaction.Date.Month()] = MonthlyTransactionSummary{
+				DataByType: make(map[model.TransactionType]SummaryTransactionType),
+			}
+		}
+
+		monthSummary := transSummaryByMonth[transaction.Date.Month()]
+		monthSummary.TransactionsCount++
+		monthSummary.Total += transaction.Value
+
+		dataByType := monthSummary.DataByType[transaction.Type]
+		dataByType.TransactionsCount++
+		dataByType.Total += transaction.Value
+
+		monthSummary.DataByType[transaction.Type] = dataByType
+
+		transSummaryByMonth[transaction.Date.Month()] = monthSummary
+	}
+
+	return transSummaryByMonth
 }
