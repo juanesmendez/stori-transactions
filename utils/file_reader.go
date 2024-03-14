c525326e4bb2d19e6a5b2188c95233882ebd2cfa
Juan.Mendez <juan.mendez@rappi.com>
2024-03-13 23:50:22 -0500

    Improved csv file reader func.

--- utils/file_reader.go
+++ utils/file_reader.go
@@ -12,19 +12,69 @@ import (
 	"time"
 )
 
-func ReadTransactions(fileName string) []model.Transaction {
-	currentDir, err := os.Getwd()
+func ReadTransactionsFromCsv(fileName string) []model.Transaction {
+	records, err := getRecords(fileName)
+
 	if err != nil {
-		fmt.Println("error getting current working directory:", err)
 		return []model.Transaction{}
 	}
 
-	absolutePath := filepath.Join(currentDir, fileName)
+	var transactions []model.Transaction
 
+	for i, record := range records {
+		if len(record) != 3 {
+			continue
+		}
+		transactionId, err := getTransactionId(record, i)
+
+		if err != nil {
+			continue
+		}
+
+		date, err := getDate(record, i)
+
+		if err != nil {
+			continue
+		}
+
+		transactionType, err := getTransactionType(record, i)
+
+		if err != nil {
+			continue
+		}
+
+		transactionValue, err := getTransactionValue(record, i)
+
+		if err != nil {
+			continue
+		}
+
+		transaction := model.Transaction{
+			ID:    transactionId,
+			Date:  date,
+			Value: transactionValue,
+			Type:  transactionType,
+		}
+
+		transactions = append(transactions, transaction)
+	}
+
+	return transactions
+}
+
+func getRecords(fileName string) ([][]string, error) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("error getting current working directory:", err)
+		return [][]string{}, err
+	}
+
+	absolutePath := filepath.Join(currentDir, fileName)
 	file, err := os.Open(absolutePath)
 
 	if err != nil {
-		log.Fatal("error reading the transactions csv file: ", err)
+		log.Printf("error reading the transactions csv file: %s", err.Error())
+		return [][]string{}, err
 	}
 
 	defer file.Close()
@@ -34,74 +84,75 @@ func ReadTransactions(fileName string) []model.Transaction {
 	recordsWithHeader, err := reader.ReadAll()
 
 	if err != nil {
-		fmt.Println("error reading transaction records from csv file")
+		log.Printf("error reading transaction records from csv file: %s", err.Error())
+		return [][]string{}, err
 	}
 
-	records := recordsWithHeader[1:]
-	var transactions []model.Transaction
+	return recordsWithHeader[1:], nil
+}
 
-	for i, record := range records {
-		if len(record) == 3 {
-			transactionIdStr := record[0]
-			transactionId, err := strconv.ParseUint(transactionIdStr, 10, 64)
+func getTransactionId(record []string, rowNum int) (uint64, error) {
+	transactionIdStr := record[0]
+	transactionId, err := strconv.ParseUint(transactionIdStr, 10, 64)
 
-			if err != nil {
-				fmt.Printf("error parsing transaction's 'id' for record #%d in csv file:%s\n", i, err.Error())
-				continue
-			}
-
-			dateStr := record[1]
-			dateParts := strings.Split(dateStr, "/")
-
-			var date time.Time
-			if len(dateParts) == 2 {
-				month, err := strconv.Atoi(dateParts[0])
-
-				if err != nil {
-					fmt.Printf("error parsing transaction's month for record #%d in csv file\n", i)
-					continue
-				}
-
-				day, err := strconv.Atoi(dateParts[1])
-
-				if err != nil {
-					fmt.Printf("error parsing transaction's day of month for record #%d in csv file\n", i)
-					continue
-				}
-
-				date = time.Date(time.Now().Year(), time.Month(month), day, 0, 0, 0, 0, time.UTC)
-			}
-
-			transactionValueStr := record[2]
-			transSign := string(transactionValueStr[0])
-
-			var transactionType model.TransactionType
-			if transSign == "+" {
-				transactionType = model.Credit
-			} else if transSign == "-" {
-				transactionType = model.Debit
-			} else {
-				continue //FIXME
-			}
-
-			transactionValue, err := strconv.ParseFloat(transactionValueStr, 64)
-
-			if err != nil {
-				fmt.Printf("error parsing transaction's value for record #%d in csv file\n", i)
-				continue
-			}
-
-			transaction := model.Transaction{
-				ID:    transactionId,
-				Date:  date,
-				Value: transactionValue,
-				Type:  transactionType,
-			}
-
-			transactions = append(transactions, transaction)
-		}
-
+	if err != nil {
+		log.Printf("error parsing transaction's 'id' for record #%d in csv file:%s\n", rowNum, err.Error())
+		return transactionId, err
 	}
 
-	return transactions
+	return transactionId, nil
+}
+
+func getDate(record []string, rowNum int) (time.Time, error) {
+	dateStr := record[1]
+	dateParts := strings.Split(dateStr, "/")
+
+	if len(dateParts) != 2 {
+		return time.Time{}, fmt.Errorf("'date' field of record #%d is  badly formatted: %s", rowNum, dateParts)
+	}
+
+	month, err := strconv.Atoi(dateParts[0])
+
+	if err != nil {
+		log.Printf("error parsing transaction's month for record #%d in csv file\n", rowNum)
+		return time.Time{}, err
+	}
+
+	day, err := strconv.Atoi(dateParts[1])
+
+	if err != nil {
+		log.Printf("error parsing transaction's day of month for record #%d in csv file\n", rowNum)
+		return time.Time{}, err
+	}
+
+	date := time.Date(time.Now().Year(), time.Month(month), day, 0, 0, 0, 0, time.UTC)
+
+	return date, nil
+}
+
+func getTransactionType(record []string, rowNum int) (model.TransactionType, error) {
+	transactionValueStr := record[2]
+	transactionSign := string(transactionValueStr[0])
+
+	if transactionSign == "+" {
+		return model.Credit, nil
+	}
+
+	if transactionSign == "-" {
+		return model.Debit, nil
+	}
+
+	return "", fmt.Errorf("'transaction' field of record #%d is  badly formatted: %s", rowNum, transactionValueStr)
+}
+
+func getTransactionValue(record []string, rowNum int) (float64, error) {
+	transactionValueStr := record[2]
+	transactionValue, err := strconv.ParseFloat(transactionValueStr, 64)
+
+	if err != nil {
+		log.Printf("error parsing transaction's value for record #%d in csv file\n", rowNum)
+		return transactionValue, err
+	}
+
+	return transactionValue, nil
 }
